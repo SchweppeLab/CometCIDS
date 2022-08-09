ae49744b71d0492d84d87998b6da43809b19ec37
Phillip Seitzer <phillipseitzer@calicolabs.com>
2022-08-08 17:35:26 -0700

    Issue 1, 10: Clean out map in preparation for depositing enormous lookup table.

--- comet-ms/CometSearch/PeptideFragmentation/MakeMS2/FragmentModels/HyperFragModel.h
+++ comet-ms/CometSearch/PeptideFragmentation/MakeMS2/FragmentModels/HyperFragModel.h
@@ -17,6 +17,12 @@ class HyperFragLookupTable {
 public:
 
     std::vector<std::vector<double>> getIntensities(unsigned int numDeuteria, unsigned int peptideLength) {
+
+        if (numDeuteria > peptideLength) {
+            //when there are more deuteria than amino acids, assume one deuterium per AA.
+            return hyperFragLookupTable.at(std::make_pair(peptideLength, peptideLength));
+        }
+
         return hyperFragLookupTable.at(std::make_pair(numDeuteria, peptideLength));
     }
 
@@ -28,8 +34,12 @@ private:
     std::map<std::pair<unsigned int, unsigned int>, std::vector<std::vector<double>>> hyperFragLookupTable;
 
     void init_map() {
+
         hyperFragLookupTable = std::map<std::pair<unsigned int, unsigned int>, std::vector<std::vector<double>>>();
-        hyperFragLookupTable.insert(std::make_pair(std::make_pair(8, 17), std::vector<std::vector<double>>{{0.5294118,0.4705882,0,0,0,0,0,0,0},{0.2647059,0.5294118,0.2058824,0,0,0,0,0,0},{0.1235294,0.4235294,0.3705882,0.08235294,0,0,0,0,0},{0.05294118,0.2823529,0.4235294,0.2117647,0.02941176,0,0,0,0},{0.02036199,0.1628959,0.3800905,0.3257919,0.10181,0.009049774,0,0,0},{0.00678733,0.08144796,0.2850679,0.3800905,0.2036199,0.04072398,0.002262443,0,0},{0.00185109,0.03455368,0.1814068,0.3628137,0.3023447,0.103661,0.01295763,0.0004113534,0},{0.000370218,0.01184698,0.09675031,0.2902509,0.3628137,0.1935006,0.04146442,0.002961744,4.113534e-05},{4.113534e-05,0.002961744,0.04146442,0.1935006,0.3628137,0.2902509,0.09675031,0.01184698,0.000370218},{0,0.0004113534,0.01295763,0.103661,0.3023447,0.3628137,0.1814068,0.03455368,0.00185109},{0,0,0.002262443,0.04072398,0.2036199,0.3800905,0.2850679,0.08144796,0.00678733},{0,0,0,0.009049774,0.10181,0.3257919,0.3800905,0.1628959,0.02036199},{0,0,0,0,0.02941176,0.2117647,0.4235294,0.2823529,0.05294118},{0,0,0,0,0,0.08235294,0.3705882,0.4235294,0.1235294},{0,0,0,0,0,0,0.2058824,0.5294118,0.2647059},{0,0,0,0,0,0,0,0.4705882,0.5294118},{0,0,0,0,0,0,0,0,1}}));
+
+
+
+
     }
 
     //private instantiation, since this is a singleton.
